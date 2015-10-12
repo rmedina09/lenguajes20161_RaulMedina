b9251a6f4f415ef575858665132e01c9996b3b2b
rmedina09 <raulmp@ciencias.unam.mx>
2015-10-11 23:47:30 -0500

    With* aun con problemas

--- Practicas/practica04/practica4.rkt
+++ Practicas/practica04/practica4.rkt
@@ -23,6 +23,14 @@
     [else (type-case Binding (car lbind)
             [bind (name val) (cons (desugar val) (list-value (cdr lbind)))])]))
 
+;;
+(define (lfae-value lbind)
+  (cond
+    [(empty? lbind) empty]
+    [else (type-case Binding (car lbind)
+            [bind (name val) (cons val (lfae-value (cdr lbind)))])]))
+
+
 ;; list-fae: listof-FAES --> listof-FAE
 ;; Dada una lista de FAES regresa una lista de FAE
 (define (list-fae l)
@@ -30,15 +38,31 @@
     [(empty? l) empty]
     [else (cons (desugar (car l)) (list-fae (cdr l)))]))
 
+;; desugar-args: symbol listof-bindings
+;;
+(define (get-value s lb)
+  (cond
+    [(empty? lb) (error 'desugar-args "no tiene valor")]
+    [else (type-case Binding (car lb)
+            [bind (name val) (if (symbol=? s name) val (get-value s (cdr lb)))])]))
+
 ;; list-Value-App: listof-Binding-FAES --> listof-FAE-app
 ;;
-(define (list-Value-App lb)
-  (cond
-    [(empty? lb) empty]
-    [else (type-case Binding (car lb)
-            [bind (name val) (cons (app (fun (list name) (id name)) (list (desugar val))) (list-Value-App (cdr lb)))])]))
+(define (value-app fs lb)
+  (type-case FAES fs
+            [idS (n) (app (fun (list n) (id n)) (list (value-app (get-value n lb) lb)))]
+            [binopS (f l r) (binop f (value-app l lb) (value-app r lb))]
+            [else (desugar fs)]))
 
-(define lbds (list (bind 'x (numS 3)) (bind 'y (numS 6))))
+(define (list-value-app lv lb)
+  (cond
+    [(empty? lv) empty]
+    [else (cons (value-app (car lv) lb) (list-value-app (cdr lv) lb))]))
+
+  
+
+;(define lbds (list (bind 'x (numS 3)) (bind 'y (binopS + (numS 2) (idS 'x)))))
+;(define lv (list (numS 3) (binopS + (numS 2) (idS 'x))))
 ;(test (list-name lbds) '(x y))
 ;(test (list-value lbds) (list (numS 3) (numS 6)))
 ;(test (parse '{with {{x {+ 5 5}}} x}) (withS (list (bind 'x (binopS + (numS 5) (numS 5)))) (idS 'x)))
@@ -51,13 +75,14 @@
   (type-case FAES expr
     [numS (n) (num n)]
     [withS (bindings body) (app (fun (list-name bindings) (desugar body)) (list-value bindings))]
-    [with*S (bindings body) (app (fun (list-name bindings) (desugar body)) (list-Value-App bindings))]
+    [with*S (bindings body) (app (fun (list-name bindings) (desugar body)) (list-value-app (lfae-value bindings) (reverse bindings)))]
     [idS (name) (id name)]
     [funS (params body) (fun params (desugar body))]
     [appS (fun args) (app (desugar fun) (list-fae args))]
     [binopS (f l r) (binop f (desugar l) (desugar r))]))
   
 ;;Test
+
 ;(test (desugar (parse 3)) (num 3))
 ;(test (desugar (parse '{+ 3 4})) (binop + (num 3) (num 4)))
 ;(test (desugar (parse '{+ {- 3 4} 7})) (binop + (binop - (num 3) (num 4)) (num 7)))
@@ -97,13 +122,16 @@
     [fun (params body) (closureV params body env)]
     [app (fun args) (let* ([fun-val (interp fun env)]
                            [params-fun (closureV-param fun-val)]
-                           [args-val (get-argsV args env)]
-                           [e (get-env (reverse params-fun) (reverse args-val) env)])
-                      (interp (closureV-body fun-val) e))]
+                           [args-val (get-argsV args env)])                           
+                      (type-case FAE-Value fun-val
+                        [closureV (param body e) (interp (closureV-body fun-val) (get-env (reverse params-fun) (reverse args-val) e))]
+                        [else (error 'interp "can only apply functions")]))]
     [binop (f l r) (binop-value f (interp l env) (interp r env))]))
 
-  ;; Implementar interp
-  ;; (error 'interp "Not implemented"))
+
+;=======================================================================
+; Funciones auxiliares
+;=======================================================================
 
 ;; get-argsV: listof-FAE Env --> listof-FAE-Value
 ;;
@@ -127,6 +155,7 @@
 (define (rinterp expr)
   (interp expr (mtSub)))
 
+(test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}}} y})) (numV 5))
 ;(test/exn (rinterp (cparse '{{fun {x y} y} 3 {+ 2 x}})) "lookup: free identifier")
 ;(test (rinterp (cparse '3)) (numV 3))
 ;(test (rinterp (cparse '{+ 3 4})) (numV 7))
@@ -147,6 +176,6 @@
 ;(test (rinterp (cparse '{with {{x 1} {y 2} {z 3}} {+ {+ x y} z}})) (numV 6))
 ;(test (rinterp (cparse '{{fun {x y z} {+ {+ x y} z}} 1 2 3})) (numV 6))
 (test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}} {z {+ x y}}} z})) (numV 8))
-;(test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}} {x 10} {z {+ x y}}} z})) (numV 15))
+(test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}} {x 10} {z {+ x y}}} z})) (numV 15))
 ;(test/exn (rinterp (cparse '{with {{x 10} {x 20}} x})) "El id x está repetido")
 ;(test (rinterp (cparse '{with* {{x 10} {x 20}} x})) (numV 20))
